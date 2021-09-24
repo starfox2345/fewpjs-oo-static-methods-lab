268eb6ed60288880530fcb4cf88509e2cc067130
yatsun <yatsunlam25@gmail.com>
2021-09-23 22:58:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
+  static capitalize(letter){
+    return letter.charAt(0).toUpperCase() + letter.slice(1);
+  }
   //add static methods here
+
+  static sanitize(letter){
+    return letter.replace(/[^A-Za-z0-9 '-]+/g, '');
+  }
+
+  //'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'
+  static titleize(word){
+    let wordSplit = word.split(" ");
+    let arr = [];
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    
+    for (let i = 0; i < wordSplit.length; i++) {
+      if (i === 0) {
+        arr.push(this.capitalize(wordSplit[i]))
+      } else {
+          if (exceptions.includes(wordSplit[i])){
+            arr.push(wordSplit[i])
+      } else { 
+        arr.push(this.capitalize(wordSplit[i]))
+      }
+      }
+    }
+    return arr.join(" ");
+
+  }
 }
